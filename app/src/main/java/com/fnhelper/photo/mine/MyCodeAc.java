176ac7560346873f6c7130e4edb961a507309185
GodOfSP <listen2375@gmail.com>
2018-10-26 09:47:36 +0800

    10/26

---
+++ app/src/main/java/com/fnhelper/photo/mine/MyCodeAc.java
@@ -0,0 +1,18 @@
+package com.fnhelper.photo.mine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fnhelper.photo.R;
+
+/**
+ *   我的code
+ */
+public class MyCodeAc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_code);
+    }
+}
