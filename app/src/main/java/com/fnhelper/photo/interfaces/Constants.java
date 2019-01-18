5c56b14eb658f4eff5915ce8e3cd013ab0e6571b
GodOfSP <listen2375@gmail.com>
2019-01-18 17:42:37 +0800

    1/18 gs

--- app/src/main/java/com/fnhelper/photo/interfaces/Constants.java
+++ app/src/main/java/com/fnhelper/photo/interfaces/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static String WECHAT_APPID="wx04ff3ceebc3fc690";
     public static String WECHAT_SECRET="661c716beceb8f6fd9cde7cff3b456d9";
     // 服务器根地址
-    public static final String ROOT_PATH = "http://photo.fengniao100.com/open/";
+    public static final String ROOT_PATH = "http://photo.ccy100.com/open/";
     //sp name
     public static String sp_name ="fn_sp";
     public static String code ="code";
